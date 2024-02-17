f9c2ec5504b25071ae49ea0028bd956786ef7004
Lorenzo Fernandez <105619839+Lorenzofernz@users.noreply.github.com>
2024-02-17 13:22:42 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,38 @@
+package com.example.demo.student;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+
+    public List<Student> getStudents() {
+        return studentRepository.findAll();
+    }
+
+    @Transactional
+    public void addNewStudent(Student student) {
+        Optional<Student> studentByEmail = studentRepository.findStudentByEmail(student.getEmail());
+        if (studentByEmail.isPresent()){
+            throw new IllegalStateException("email already taken");
+        }
+
+        studentRepository.save(student);
+        System.out.println(student);
+    }
+}
